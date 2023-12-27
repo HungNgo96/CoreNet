9bbd29445f6069d1de40e6286735edc10df12123
HungNT145 <HungNT145@fpt.com>
2023-12-27 23:13:05 +0700

    Add project files.

---
+++ Application/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjectionExtension).Assembly;
+
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(assembly);
+            });
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
---
+++ Presentation/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
