8235568219ab3ca543564ddc5a979f767dea2d69
HungNT145 <HungNT145@fpt.com>
2023-12-30 22:50:41 +0700

    adding new file editorconfig

---
+++ Application/Behaviors/UnitOfWorkBehavior.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Application.Data;
+using MediatR;
+
+namespace Application.Behaviors
+{
+    public sealed class UnitOfWorkBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UnitOfWorkBehavior(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<TResponse> Handle(TRequest request,
+                                      RequestHandlerDelegate<TResponse> next,
+                                      CancellationToken cancellationToken)
+        {
+            if (IsNotCommand())
+            {
+                return await next();
+            }
+
+            using (var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var response = await next();
+
+                await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                transactionScope.Complete();
+
+                return response;
+            }
+        }
+
+        private static bool IsNotCommand()
+        {
+            return !typeof(TRequest).Name.EndsWith("Command");
+        }
+    }
+}
---
+++ Application/Data/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Application.Data
+{
+    public interface IUnitOfWork
+    {
+        public Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
