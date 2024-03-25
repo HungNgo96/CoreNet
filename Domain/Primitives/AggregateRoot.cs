ecfbbb16f726d955f5284ff9aaac070325e0c72f
HungNT145 <HungNT145@fpt.com>
2024-03-25 22:42:00 +0700

    [DomainEvent] - setup

---
+++ Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DomainEvents;
+
+namespace Domain.Primitives
+{
+    public abstract class AggregateRoot
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+    }
+}
