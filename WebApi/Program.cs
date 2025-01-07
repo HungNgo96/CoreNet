b01d4a4ab5144494423384a72109979a5f37b2fc
HungNT97 <HungNT97@fpt.com>
2025-01-07 09:09:30 +0700

    update file Docker-compose

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -92,7 +92,7 @@ if (app.Environment.IsDevelopment())
     using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<WriteApplicationDbContext>();
 
-    if (!await dbContext.Database.CanConnectAsync(default))
+    if (!await dbContext.Database.CanConnectAsync(CancellationToken.None))
     {
         throw new ConnectionException("Couldn't connect database.");
     }
